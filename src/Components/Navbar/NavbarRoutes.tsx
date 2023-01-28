77a4c90950b618e7d97866af464e242acf2bcea1
Sunil Joshi <suniljoshi2621@gmail.com>
2023-01-28 19:12:51 +0530

    active class added

--- src/Components/Navbar/NavbarRoutes.tsx
+++ src/Components/Navbar/NavbarRoutes.tsx
@@ -22,7 +22,7 @@ onClick={()=> {
 }
 }}
  key={item.to} >
-          <Link to={item.to} className={`block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400  dark:hover:bg-gray-700 md:dark:hover:bg-transparent  ${pathname===item.to  || pathname.includes("tvseries") && item.routeName.includes("TV") || pathname.includes("movies") &&  item.routeName.includes("Movies") ? "md:text-blue-700":""}` }>{item.routeName}</Link>
+          <Link to={item.to} className={`block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400  dark:hover:bg-gray-700 md:dark:hover:bg-transparent  ${pathname===item.to  || pathname.includes("tvseries") && item.routeName.includes("TV") || pathname.includes("movies") &&  item.routeName.includes("Movies") ? "text-blue-700":""}` }>{item.routeName}</Link>
         </li>
 })
 	}
