a4fedc374b3a2d70c60679a5fb98e28cd0330dd8
Sunil Joshi <suniljoshi2621@gmail.com>
2023-01-28 17:12:36 +0530

    modal close

--- src/utility/Modal.tsx
+++ src/utility/Modal.tsx
@@ -11,10 +11,10 @@ const {showModal,setShowModal,loader,setLoader,videoData} = props;
       {showModal ? (
         <>
           <div
-	
+	onClick={() => setShowModal(false)}
             className="justify-center  items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div 	  tabIndex={-1} className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div      tabIndex={-1} className="relative w-auto my-6 mx-auto max-w-3xl">
 
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-2 border-b border-solid border-slate-200 rounded-t">
