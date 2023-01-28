0d0f726ac0f53aa62596372c6f2e86d669b6037d
Sunil Joshi <suniljoshi2621@gmail.com>
2023-01-28 19:28:42 +0530

    md classes added

--- src/Components/Navbar/NavbarComponent.tsx
+++ src/Components/Navbar/NavbarComponent.tsx
@@ -10,7 +10,7 @@ function NavbarComponent(){
 	
 return(
 
- <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900" >
+ <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 md:dark:bg-gray-800" >
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="https://iamsunil25.github.io/portfolio" className="flex items-center">
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">iamsunil25</span>
