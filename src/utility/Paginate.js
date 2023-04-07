dbf875290bf1eb53b3d5b7e13f3cd855d0e51ffe
Sunil Joshi <suniljoshi2621@gmail.com>
2023-04-08 02:00:38 +0530

    pagination reduce to 30

--- src/utility/Paginate.js
+++ src/utility/Paginate.js
@@ -26,7 +26,7 @@ return(
 
     <li   style={{pointerEvents:page=='1' ? 'none':'', cursor:page=='1' ? 'default':'pointer'}}  onClick={previousPage}  className="px-3 py-2 ml-0 leading-tight text-gray-500 border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</li>
 	{
-		 Array.from({length:30},()=>'filled').map((item,index)=><li key={index+1} onClick={()=>setPage(index+1)} style={{cursor:'pointer'}}  className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${page==index+1 ? 'activePage':''}`}>{index+1}</li>)
+		 Array.from({length:27},()=>'filled').map((_,index)=><li key={index+1} onClick={()=>setPage(index+1)} style={{cursor:'pointer'}}  className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${page==index+1 ? 'activePage':''}`}>{index+1}</li>)
 	}
 
     <li  style={{pointerEvents:page=='30' ? 'none':'', cursor:page=='30' ? 'default':'pointer'}}  onClick={nextPage}
