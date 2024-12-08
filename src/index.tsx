eb23ed4daeddc814fb72c06a2aee73ce6aef00f0
Sunil Joshi <55904928+iamsunil25@users.noreply.github.com>
2024-12-08 22:14:04 +0530

    persist added with redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import store from './redux/store';
+import store, { persistor } from './redux/store'; // Import persistor
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-	 <Provider store={store}>
-	<BrowserRouter>
-    <App />
-	</BrowserRouter>
-	</Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favouriteMovies from './favouriteMovies';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Use local storage for persistence
+import { combineReducers } from 'redux';
 
-export default configureStore({
-  reducer: {
-    favouriteMovies: favouriteMovies,
-  },
+// Combine all reducers
+const rootReducer = combineReducers({
+  favouriteMovies,
 });
+
+// Persist configuration
+const persistConfig = {
+  key: 'root', // Key for localStorage
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Export the store and persistor
+export const persistor = persistStore(store);
+export default store;
